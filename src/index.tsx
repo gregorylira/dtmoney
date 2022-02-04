5b3adef0f29bd09c704edb4ea8a5a3da17d3be89
gregorylira <gregory.lira007@gmail.com>
2022-02-04 18:01:52 -0300

    adicionando backend mockado

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createServer } from "miragejs";
 import App from "./App";
+
+createServer({
+  routes() {
+    this.namespace = "/api";
+
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "Desenvolvimento de website",
+          amount: 12000,
+          category: "Desenvolvimento",
+          type: "deposit",
+          createdAt: new Date(),
+        },
+      ];
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
